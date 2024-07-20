1aad3e4855652aca96f8b33651d3814df51674db
ktag3000 <136417976+ktag3000@users.noreply.github.com>
2024-07-19 20:20:50 -0500

    Create worker logic
    Timer will run even when browser tab is inactive

--- script.js
+++ script.js
@@ -3,83 +3,39 @@ let startBtn = document.getElementById("start");
 let stopBtn = document.getElementById("stop");
 let resetBtn = document.getElementById("reset");
 
-let hour = 00;
-let minute = 00;
-let second = 00;
-let count = 00;
+// Create Web Worker
+let worker = new Worker("worker.js");
+
+// Listen for messages from the worker
+worker.onmessage = function (e) {
+  let { hour, minute, second, count } = e.data;
+
+  let hrString = hour < 10 ? "0" + hour : hour;
+  let minString = minute < 10 ? "0" + minute : minute;
+  let secString = second < 10 ? "0" + second : second;
+  let countString = count < 10 ? "0" + count : count;
+
+  document.getElementById("hr").innerHTML = hrString;
+  document.getElementById("min").innerHTML = minString;
+  document.getElementById("sec").innerHTML = secString;
+  document.getElementById("count").innerHTML = countString;
+};
 
 // Button click events
 startBtn.addEventListener("click", function () {
-  timer = true;
-  stopWatch();
+  worker.postMessage("start");
   startBtn.style.display = "none";
   stopBtn.style.display = "block";
 });
 
 stopBtn.addEventListener("click", function () {
-  timer = false;
+  worker.postMessage("stop");
   startBtn.style.display = "block";
   stopBtn.style.display = "none";
 });
 
 resetBtn.addEventListener("click", function () {
-  timer = false;
-  hour = 0;
-  minute = 0;
-  second = 0;
-  count = 0;
-  document.getElementById("hr").innerHTML = "00";
-  document.getElementById("min").innerHTML = "00";
-  document.getElementById("sec").innerHTML = "00";
-  document.getElementById("count").innerHTML = "00";
+  worker.postMessage("reset");
+  startBtn.style.display = "block";
+  stopBtn.style.display = "none";
 });
-
-// Stopwatch function
-function stopWatch() {
-  if (timer) {
-    count++;
-
-    if (count == 100) {
-      second++;
-      count = 0;
-    }
-
-    if (second == 60) {
-      minute++;
-      second = 0;
-    }
-
-    if (minute == 60) {
-      hour++;
-      minute = 0;
-      second = 0;
-    }
-
-    let hrString = hour;
-    let minString = minute;
-    let secString = second;
-    let countString = count;
-
-    if (hour < 10) {
-      hrString = "0" + hrString;
-    }
-
-    if (minute < 10) {
-      minString = "0" + minString;
-    }
-
-    if (second < 10) {
-      secString = "0" + secString;
-    }
-
-    if (count < 10) {
-      countString = "0" + countString;
-    }
-
-    document.getElementById("hr").innerHTML = hrString;
-    document.getElementById("min").innerHTML = minString;
-    document.getElementById("sec").innerHTML = secString;
-    document.getElementById("count").innerHTML = countString;
-    setTimeout(stopWatch, 10);
-  }
-}
